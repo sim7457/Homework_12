a14196e7a569134cc122e6de6cfb7c4e5f7ca01b
sim7457 <118698648+sim7457@users.noreply.github.com>
2023-02-06 17:44:40 +0900

    230206 17:44

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.notice_slick').slick({
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+    });
+
+    $('.notice-box i').on('click', function () {
+        $('.notice-box').addClass('on')
+    });
+});
