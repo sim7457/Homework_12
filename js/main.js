b4cb0ebd3b5d0863a6a65f149dde4460d653ab27
sim7457 <118698648+sim7457@users.noreply.github.com>
2023-02-08 17:43:52 +0900

    230208 17:43

--- js/main.js
+++ js/main.js
@@ -9,15 +9,31 @@ $(function () {
         autoplay: true,
         autoplaySpeed: 3000,
         dots: true,
+        arrows: false,
     });
 
+    $('.sub_slide').slick({
+        lazyLoad: 'ondemand',
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
 
-
-
+    $('.img_slide').slick({
+        arrows: false,
+        fade: true,
+    });
 
     $('.notice-box i').on('click', function () {
         $('.notice-box').addClass('on')
     });
 
+    $('.tab-list span').on('click', function () {
+        const idx = $(this).index();
+        $(this).addClass('on').siblings().removeClass('on');
+        $('.img_slide').slick('slickGoTo', idx);
+    });
 
 });
